bea4d83d2671c46058d7eef0f14bc4d436d72a4d
KhaviyaSree <khaviyasreem@gmail.com>
2025-09-21 16:09:17 +0530

    Add Donor+Donation

---
+++ NGO-Management/NGO Donation and Volunteer Management System/src/ngo/Main.java
@@ -0,0 +1,48 @@
+package ngo;
+
+import java.util.*;
+
+public class Main {
+    private static Scanner sc=new Scanner(System.in);
+    private static List<Donor> donors=new ArrayList<>();
+    private static List<Donation> donations=new ArrayList<>();
+    private static List<Volunteer> volunteers=new ArrayList<>();
+    private static List<Event> events=new ArrayList<>();
+
+    private static int donorIdCounter=1;
+    private static int donationIdCounter=1;
+    private static int volunteerIdCounter=1;
+    private static int eventIdCounter=1;
+
+    public static void main(String[] args) {
+        System.out.println("=== NGO Donation & Volunteer Management System ===");
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1.Register Donor");
+            System.out.println("2.Make Donation");
+            System.out.println("3.Register Volunteer");
+            System.out.println("4.Create Event");
+            System.out.println("5.Assign Volunteer to Event");
+            System.out.println("6.Generate Reports (Admin)");
+            System.out.println("7.Exit");
+
+            int choice = sc.nextInt(); sc.nextLine();
+
+            switch (choice) {
+                case 1 ->registerDonor();
+                case 2 ->makeDonation();
+                case 3 ->registerVolunteer();
+                case 4 ->createEvent();
+                case 5 ->assignVolunteer();
+                case 6 ->generateReports();
+                case 7 -> {
+                    running = false;
+                    System.out.println("Goodbye!");
+                }
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+    }
+    
