e6131af01d97653388eeae3eab02c9f7da3119e2
KhaviyaSree <khaviyasreem@gmail.com>
2025-09-21 14:47:42 +0530

    Event.java added

---
+++ NGO-Management/NGO Donation and Volunteer Management System/src/ngo/Event.java
@@ -0,0 +1,34 @@
+package ngo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    private int eventId;
+    private String eventName;
+    private String date;
+    private String location;
+    private List<Volunteer> volunteers = new ArrayList<>();
+
+    public Event(int eventId, String eventName, String date, String location) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.date = date;
+        this.location = location;
+    }
+
+    public void assignVolunteer(Volunteer volunteer) {
+        volunteers.add(volunteer);
+        System.out.println("Volunteer " + volunteer.sname + " assigned to event " + eventName);
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    @Override
+    public String toString() {
+        return "Event ID: " + eventId + ", Name: " + eventName + ", Date: " + date + ", Location: " + location;
+    }
+}
+
