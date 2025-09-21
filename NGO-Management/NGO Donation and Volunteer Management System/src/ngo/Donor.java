eff480d1f1ff7dda4c0b08d23b0245e6d4a50c8a
KhaviyaSree <khaviyasreem@gmail.com>
2025-09-21 15:15:25 +0530

    Donor.java modified

--- NGO-Management/NGO Donation and Volunteer Management System/src/ngo/Donor.java
+++ NGO-Management/NGO Donation and Volunteer Management System/src/ngo/Donor.java
@@ -4,56 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Donor extends User {
-    //Donation class
-    public class Donation {
-        private double amount;
-
-        public Donation(double amount) {
-            this.amount = amount;
-        }
-
-        public double getAmount() {
-            return amount;
-        }
-
-        @Override
-        public String toString() {
-            return "Donation: Rs." + amount;
-        }
-    }
-
     private double totaldonated;
-    private List<Donation> donations = new ArrayList<>();
+    private List<Donation> donations=new ArrayList<>();
 
-    public Donor(int iuserId, String sname, String semail, String sphone) {
+    public Donor(int iuserId,String sname,String semail,String sphone) {
         super(iuserId, sname, semail, sphone);
         this.totaldonated = 0;
     }
 
     public void makeDonation(Donation donation) {
         donations.add(donation);
-        totaldonated += donation.getAmount();
-        System.out.println(sname + " donated Rs." + donation.getAmount());
+        totaldonated +=donation.getAmount();
+        System.out.println(sname+" donated Rs."+donation.getAmount());
     }
 
     public void viewDonationHistory() {
         System.out.println("Donation History for " + sname + ":");
-        for (Donation d : donations) {
+        for (Donation d:donations) {
             System.out.println(d);
         }
     }
 
     public double getTotaldonated() {
         return totaldonated;
-    }
-
-    public static void main(String[] args) {
-        Donor donor = new Donor(2,"Oviya", "oviya@gmail.com", "986543210");
-        Donor.Donation donation1 = donor.new Donation(1000);
-        Donor.Donation donation2 = donor.new Donation(500);
-        donor.makeDonation(donation1);
-        donor.makeDonation(donation2);
-        donor.viewDonationHistory();
-        System.out.println("Total Donated: Rs." + donor.getTotaldonated());
     }
 }
